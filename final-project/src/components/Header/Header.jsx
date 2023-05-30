b4251b6c70d409bd1f4afa48bffe9da0e24447c5
Collin <88743846+PaizCollin@users.noreply.github.com>
2023-05-30 00:34:08 -0700

    fixes header mobile functionality
    fixes header mobile functionality and adds animatinos

--- final-project/src/components/Header/Header.jsx
+++ final-project/src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
 import { NavLink } from "react-router-dom";
 import styles from "./Header.module.css";
+import { useEffect, useState } from "react";
 
 function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMenuClick = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
   return (
     <header>
       <nav className={styles["header__nav"]}>
@@ -34,7 +41,42 @@ function Header() {
 
         <ul className={styles["header__menu-mobile"]} data-aos="fade-down">
           <li>
-            <img src="../../../public/menu.svg" alt="menu" />
+            <img
+              src="../../../public/menu.svg"
+              alt="menu"
+              onClick={handleMenuClick}
+            />
+            {isMobileMenuOpen && (
+              <ul className={styles["header__menu-mobile-dropdown"]}>
+                <li>
+                  <NavLink exact to="/" onClick={handleMenuClick}>
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/discover" onClick={handleMenuClick}>
+                    Discover
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/sellerss" onClick={handleMenuClick}>
+                    Sellers
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink to="/login" onClick={handleMenuClick}>
+                    Login
+                  </NavLink>
+                </li>
+                <li>
+                  <div className={styles["button-white"]}>
+                    <NavLink to="/signup" onClick={handleMenuClick}>
+                      Sign Up
+                    </NavLink>
+                  </div>
+                </li>
+              </ul>
+            )}
           </li>
         </ul>
       </nav>
