496eb3fec71a91189bf77248f6ba488335ae3a0d
Ben Airola <benairola@Bens-MacBook-Air.local>
2023-06-05 14:54:43 -0700

    Fixed register button.

--- final-project/src/pages/SignUp/SignUp.jsx
+++ final-project/src/pages/SignUp/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './signup.css';
+import './SignUp.module.css';
 
 const SignUp = () => {
   const [username, setUsername] = useState('');
